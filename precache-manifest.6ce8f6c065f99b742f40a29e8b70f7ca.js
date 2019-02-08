7fbe8a58cf57ee3836010d5c3eabe0487a93ca57
Kartik <kartiksinghal122@gmail.com>
2019-02-08 23:19:05 +0530

    Updates

---
+++ precache-manifest.6ce8f6c065f99b742f40a29e8b70f7ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dafb13ae6de74392672",
+    "url": "/Movie-Review---ReactJS/static/js/runtime~main.1dafb13a.js"
+  },
+  {
+    "revision": "fb48225de509420f5e02",
+    "url": "/Movie-Review---ReactJS/static/js/main.fb48225d.chunk.js"
+  },
+  {
+    "revision": "89ae401387cd4ef7ec34",
+    "url": "/Movie-Review---ReactJS/static/js/1.89ae4013.chunk.js"
+  },
+  {
+    "revision": "fb48225de509420f5e02",
+    "url": "/Movie-Review---ReactJS/static/css/main.2c190257.chunk.css"
+  },
+  {
+    "revision": "421607e0fe09126f05b6722785904369",
+    "url": "/Movie-Review---ReactJS/index.html"
+  }
+];
